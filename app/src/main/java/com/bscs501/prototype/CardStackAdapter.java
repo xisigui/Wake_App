d5fb9413cd16fda8c9ed9722fc37a181781ddb9a
xisigui <andreiulupon@gmail.com>
2020-12-28 09:26:21 +0800

    Callerpicker Swipe UI / Mainlogin Revert Color

---
+++ app/src/main/java/com/bscs501/prototype/CardStackAdapter.java
@@ -0,0 +1,64 @@
+package com.bscs501.prototype;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class CardStackAdapter extends RecyclerView.Adapter<CardStackAdapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+    private List<Spot> spots;
+    private Context mContext;
+
+    public CardStackAdapter(Context context, List<Spot> spots) {
+        this.inflater = LayoutInflater.from(context);
+        this.spots = spots;
+        this.mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.item_spot, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Spot spot = spots.get(position);
+        holder.name.setText(spot.name);
+        holder.city.setText(spot.city);
+        Glide.with(this.mContext)
+                .load(spot.url)
+                .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return spots.size();
+    }
+
+    public void addSpots(List<Spot> spots) {
+        this.spots.addAll(spots);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView city;
+        private ImageView image;
+        ViewHolder(View view) {
+            super(view);
+            this.name = view.findViewById(R.id.item_name);
+            this.city = view.findViewById(R.id.item_city);
+            this.image = view.findViewById(R.id.item_image);
+        }
+    }
+
+}
--- app/src/main/java/com/bscs501/prototype/MainActivity.java
+++ app/src/main/java/com/bscs501/prototype/MainActivity.java
@@ -1,15 +1,21 @@
 package com.bscs501.prototype;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Patterns;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,15 +34,24 @@ import com.marozzi.roundbutton.RoundButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView registraiton, forgotpassword, showbtn;
+    private TextView registraiton, forgotpassword, showbtn, k;
     private EditText editTextEmail, editTextPassword;
     private RoundButton loginbtn;
     private FirebaseAuth mAuth;
+    private View background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        background = findViewById(R.id.over);
+
+        k = findViewById(R.id.textView2);
+        k.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, activity_callerpick.class));
+            }
+        });
 
         registraiton = (TextView)findViewById(R.id.registrationbtn);
         registraiton.setOnClickListener(this);
@@ -61,11 +76,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(editTextPassword.getText().length()>0){
+                if (editTextPassword.getText().length() > 0) {
                     showbtn.setVisibility(View.VISIBLE);
-                }else{
-                    showbtn.setVisibility(View.GONE);
-                }
+                }else{showbtn.setVisibility(View.GONE);}
             }
 
             @Override
@@ -74,11 +87,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        ConstraintLayout LL = findViewById(R.id.Main);
-        AnimationDrawable AD = (AnimationDrawable) LL.getBackground();
-        AD.setEnterFadeDuration(2000);
-        AD.setExitFadeDuration(4000);
-        AD.start();
+//        ConstraintLayout LL = findViewById(R.id.Main);
+//        AnimationDrawable AD = (AnimationDrawable) LL.getBackground();
+//        AD.setEnterFadeDuration(2000);
+//        AD.setExitFadeDuration(4000);
+//        AD.start();
 
         Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake);
         wake.startAnimation(animation);
@@ -128,6 +141,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             editTextEmail.setError("Email is Empty");
             editTextEmail.requestFocus();
             loginbtn.revertAnimation();
+            editTextEmail.startAnimation(shakeError());
             return;
         }
 
@@ -135,19 +149,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             editTextEmail.setError("Please Enter a Valid Email");
             editTextEmail.requestFocus();
             loginbtn.revertAnimation();
+            editTextEmail.startAnimation(shakeError());
             return;
         }
 
         if(password.isEmpty()){
-            editTextPassword.setError("Email is empty");
+            editTextPassword.setError("Password is empty");
             editTextPassword.requestFocus();
             loginbtn.revertAnimation();
+            editTextPassword.startAnimation(shakeError());
             return;
         }
         if(password.length() < 6){
             editTextPassword.setError("Password Length is at Least 6 Characters.");
             editTextPassword.requestFocus();
             loginbtn.revertAnimation();
+            editTextPassword.startAnimation(shakeError());
             return;
         }
         mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -158,8 +175,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if(user.isEmailVerified()){
                         editTextEmail.clearComposingText();
                         editTextPassword.clearComposingText();
+                        circularRevealActivity();
                         startActivity(new Intent(MainActivity.this, Dashboard.class));
-
+                        finish();
                     }else {
                         user.sendEmailVerification();
                         Toast.makeText(MainActivity.this, "Please Check your Email to Verify your Account.", Toast.LENGTH_LONG).show();
@@ -171,6 +189,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
-
     }
+
+    private void circularRevealActivity() {
+        int cx = (background.getLeft() + background.getRight()) / 2;
+        int cy = (background.getTop() + background.getBottom()) / 2;
+        float finalRadius = Math.max(background.getWidth(), background.getHeight());
+
+        Animator circularReveal = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            circularReveal = ViewAnimationUtils.createCircularReveal(
+                    background,cx,cy,0,finalRadius);
+        }
+        circularReveal.setDuration(300);
+        background.setVisibility(View.VISIBLE);
+        loginbtn.setVisibility(View.GONE);
+        circularReveal.start();
+    }
+
+    public TranslateAnimation shakeError() {
+        TranslateAnimation shake = new TranslateAnimation(0, 15, 0, 0);
+        shake.setDuration(500);
+        shake.setInterpolator(new CycleInterpolator(3));
+        return shake;
+    }
+
 }
